f0cda7b63323368a19700d296782b0d0379eee5b
Joshua Bell <josh@joshuabell.io>
2015-04-26 01:06:48 -0400

    Add profiles.clj

---
+++ profiles.clj
@@ -0,0 +1,5 @@
+{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                  [refactor-nrepl "0.3.0-SNAPSHOT"]
+                  [lein-kibit "0.1.2"]]
+        :dependencies [[alembic "0.3.2"]
+                       [org.clojure/tools.nrepl "0.2.7"]]}}
